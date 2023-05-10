76ee1d74476bbeebbba77378960d58d494eecc36
NNhatTThien <thientryhard@gmail.com>
2023-05-10 22:09:28 +0700

    Finish login with google and github in Client server

---
+++ src/main/java/com/wolfalone/loginwithsocialbyspringboot/LoginWithSocialBySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wolfalone.loginwithsocialbyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithSocialBySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithSocialBySpringbootApplication.class, args);
+	}
+
+}
